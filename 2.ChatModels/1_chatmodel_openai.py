0993814859086376580a91df6a434dee21ef35bc
vivek Rawat <vivekrawatt.17@gmail.com>
2026-09-05 13:03:14 +0530

    Initial commit

---
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
+
+result = model.invoke("What is the capital of France?")
+
+print(result.content)
